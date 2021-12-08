4d6b04e61209514af27a350ff869c1780da469dd
Tien Ho <hodaitien972019@gmail.com>
2021-12-08 18:04:12 +0700

    create useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import Movies from './components/Movies';
 import Navbar from './components/Navbar';
 import ToggleThemeBtn from './components/ToggleThemeBtn';
+import AuthContextProvider from './contexts/AuthContext';
 import MovieContextProvider from './contexts/MovieContext';
 import ProgressContextProvider from './contexts/ProgressContext';
 import ThemeContextProvider from './contexts/ThemeContext';
@@ -10,15 +11,17 @@ import ThemeContextProvider from './contexts/ThemeContext';
 function App() {
   return (
     <div>
-      <MovieContextProvider>
-        <ThemeContextProvider>
-          <ProgressContextProvider>
-            <Navbar />
-            <Movies />
-            <ToggleThemeBtn />
-          </ProgressContextProvider>
-        </ThemeContextProvider>
-      </MovieContextProvider>
+      <AuthContextProvider>
+        <MovieContextProvider>
+          <ThemeContextProvider>
+            <ProgressContextProvider>
+              <Navbar />
+              <Movies />
+              <ToggleThemeBtn />
+            </ProgressContextProvider>
+          </ThemeContextProvider>
+        </MovieContextProvider>
+      </AuthContextProvider>
     </div>
   );
 }
